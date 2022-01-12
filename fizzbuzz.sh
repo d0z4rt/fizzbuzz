9c9986c28b6694639bc6e9fa443f5b63a2f73dc3
d0z4rt <d0z@d0z.eu>
2022-01-12 10:16:04 +0100

    fix: ignore results

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -7,29 +7,31 @@ echo "Stop num"
 # read stop
 stop=$2
 
+file=results/results.log
+
 while [ $start -le $stop ]
 do
     # Check if divide by 3 & 5
     if [[ 0 -eq "($start%3) + ($start%5)" ]] 
     then
         echo "fizz buzz"
-        echo "fizz buzz" >> results.log
+        echo "fizz buzz" >> $file
 
     # Check if divide by 5   
     elif [[ 0 -eq "($start%5)" ]]
     then
         echo "buzz"
-        echo "buzz" >> results.log
+        echo "buzz" >> $file
 
     # Check if divide by 3
     elif [[ 0 -eq "($start%3)" ]]
     then
         echo "fizz"
-        echo "fizz" >> results.log
+        echo "fizz" >> $file
 
     else
         echo "$start"
-        echo "$start" >> results.log
+        echo "$start" >> $file
 
     fi	
     start=$(( $start + 1 ))
